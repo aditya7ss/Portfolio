7688d58ded02455ca7406aee4bec72eea95eb80f
Aditya Shukla <shuklaadi26100@gmail.com>
2023-06-27 15:57:55 +0530

    changes about

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,33 +8,39 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, My name is  <span className="purple">Aditya Shukla </span>
+            Hi Everyone, My name is{" "}
+            <span className="purple">Aditya Shukla </span>
             from <span className="purple"> Rewa Madhya Pradesh.</span>
-            <br /> I am a final year student pursuing an Integrated MSc (IMSc)
-            in Maths and Computing at BIT Mesra.
-            <br />
-            Additionally, I am currently employed as a software developer at
-            Juspay.
+            <br /> I have done the BTech Engineering from
+            <span className="purple"> RUSTAM JI INSTITUTE OF TECHNOLOGY </span>a
+            four year of UG Program in{" "}
+            <span className="purple">Electronics and Communication </span>{" "}
+            Passout of 2021 batch.
+            <br />After I have strong interset in webdevlopmet so i decided to go
+            through a course of{" "}
+            <span className="purple">Full Stack Webdevlopmet</span>
+            from <span style={{ color: "black" }}>Coding Ninja </span>where I
+            learnt and enhance my knowledge on this field.
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight /> Playing Cricket/Badmintion
             </li>
             <li className="about-activity">
-              <ImPointRight /> Coding
+              <ImPointRight /> Tutoring
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling 
+              <ImPointRight /> Travelling
             </li>
-           < li className="about-activity">
-              <ImPointRight /> Cooking  
+            <li className="about-activity">
+              <ImPointRight /> Cooking
             </li>
           </ul>
 
-          <p style={{ color:'black' }}>
+          <p style={{ color: "black" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
           <footer className="blockquote-footer">Aditya</footer>
